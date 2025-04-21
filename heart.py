e34e413c75fc8416e87bccbcdb58a345a22d2bef
Shivkanya Nawale <shivkanyanawale4@gmail.com>
2025-04-21 11:10:42 +0530

    Update heart.py

--- heart.py
+++ heart.py
@@ -9,7 +9,7 @@ from firebase_admin import credentials, firestore
 
 # Firebase Initialization
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate("firebase_admin_sdk.json")
     firebase_admin.initialize_app(cred)
 db = firestore.client()
 
